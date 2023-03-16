be674086eafbf75bf2215b003d482f15c241b0d1
chenzhenhui.wayne <chenzhenhui.wayne@bytedance.com>
2023-03-16 14:27:06 +0800

    react-18-ssr增加注释

--- react-18-ssr/client/index.js
+++ react-18-ssr/client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {hydrateRoot} from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import { Hello } from '../components';
 
+// 将 hydration 的范围限定在 div#root 节点内
 hydrateRoot(document.getElementById('root'), <Hello />);
