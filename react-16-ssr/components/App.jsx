2dd2588d87b3c18a0ab0959c50ba4564e0d986ce
chenzhenhui.wayne <chenzhenhui.wayne@bytedance.com>
2023-06-19 20:46:53 +0800

    react-18-ssr增加renderToNodeStream

--- react-16-ssr/components/App.jsx
+++ react-16-ssr/components/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const App = () => {
   const handleClick = () => console.log('hello world');
 
   return <div onClick={handleClick}>hello world</div>;
+  // return <div>{Array.from(new Array(800000)).map((_, i) => <div key={i}>{i}</div>)}</div>
 };
--- react-16-ssr/server/index.js
+++ react-16-ssr/server/index.js
@@ -2,7 +2,7 @@ import * as path from 'path'
 import express from 'express';
 
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToNodeStream, renderToString } from 'react-dom/server';
 import { App } from '../components';
  
 const app = express();
@@ -12,21 +12,42 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  const content = renderToString(<App />);
-  const html = `
+  // const content = renderToString(<App />);
+  // const html = `
+  //   <html>
+  //     <head>
+  //       <meta charset="utf-8" />
+  //       <title>React SSR</title>
+  //     </head>
+  //     <body>
+  //       <div id="root">${content}</div>
+  //       <script src="bundle.js"></script>
+  //     </body>
+  //   </html>
+  // `;
+
+  // res.send(html)
+
+  res.write(`
     <html>
       <head>
         <meta charset="utf-8" />
         <title>React SSR</title>
       </head>
       <body>
-        <div id="root">${content}</div>
+        <div id="root">`);
+  const stream = renderToNodeStream(<App />);
+  // 当 stream(Readable stream) 完结后，nodejs 默认会关闭 res(Writable Stream)
+  // 设置 { end: false } 是为了禁用上述默认行为，从而继续写入内容（script 标签）
+  stream.pipe(res, { end: false });
+  stream.on('end', () => {
+    res.write(`</div>
         <script src="bundle.js"></script>
-      </body>
-    </html>
-  `;
-
-  res.send(html)
+        </body>
+      </html>
+    `);
+    res.end();
+  })
 });
 
 app.listen(3000);
