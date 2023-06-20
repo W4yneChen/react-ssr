a1ece36a31973d3f37c231860a79d557fea64aec
chenzhenhui.wayne <chenzhenhui.wayne@bytedance.com>
2023-06-20 21:14:16 +0800

    react-16-ssr增加renderToNodeStream

--- react-16-ssr/components/App.jsx
+++ react-16-ssr/components/App.jsx
@@ -3,6 +3,6 @@ import React, { useEffect, useState } from 'react';
 export const App = () => {
   const handleClick = () => console.log('hello world');
 
-  return <div onClick={handleClick}>hello world</div>;
-  // return <div>{Array.from(new Array(800000)).map((_, i) => <div key={i}>{i}</div>)}</div>
+  // return <div onClick={handleClick}>hello world</div>;
+  return <div>{Array.from(new Array(100000)).map((_, i) => <div key={i} onClick={handleClick}>{i}</div>)}</div>
 };
